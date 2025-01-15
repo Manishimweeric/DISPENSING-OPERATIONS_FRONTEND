7c756d8c67fce3a2a2122266e5f355f05c7b96a6
NaomeJoyeuse <naomejoyce5@gmail.com>
2025-01-15 20:55:05 +0200

    signup

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
+import Signup from './pages/user/signup';
 
 const App = () => {
   return (
     <Router>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   </Router>
   );
